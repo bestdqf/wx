df995bb4d6b00a43160007d359a5391213099a07
duanqifeng <duanqifeng@jd.com>
2022-09-22 18:10:26 +0800

    t

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
-const Koa = require('koa');
+const express = require('express')
+const app = express()
 
-// 创建一个Koa对象表示web app本身:
-const app = new Koa();
+app.get('/', (req, res) => {
+  res.send('欢迎使用微信云托管！')
+})
 
-// 对于任何请求，app将调用该异步函数处理请求：
-app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
-});
+app.get('/user', (req, res) => {
+  res.send('{name:"dqf"}')
+})
 
-// 在端口3000监听:
-app.listen(80);
-console.log('app started at port 3000...');
+const port = process.env.PORT || 80
+app.listen(port, () => {
+  console.log('服务启动成功，端口：', port)
+})
